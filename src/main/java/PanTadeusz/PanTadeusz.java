dbcfb2c2a6771285bbe3a6e2e08d0c3ec714b0e7
vanndhall <szymon.mantey@wp.pl>
2019-03-17 15:24:27 +0100

    Program  do wyszukiwania słów oraz ich liczenia/sumowania

--- src/main/java/PanTadeusz/PanTadeusz.java
+++ src/main/java/PanTadeusz/PanTadeusz.java
@@ -5,19 +5,25 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PanTadeusz {
     public static void main(String[] args) throws IOException {
         final File file = new File("C:\\Users\\VanndDhall\\Desktop\\pan-tadeusz.txt");
        final String text = FileUtils.readFileToString(file, Charset.defaultCharset());
-        final String[] words = text.split("[ \t\n,.;:\"'  \\-()/...!?«»]+");
+        final String[] words = text.split("[ \t\n,.;:\"'  \\-()/...!?«»]+—");
+        final String[] lowerCaseWords = new  String[words.length];
+
+        for(int i = 0; i<words.length;i++){
+            lowerCaseWords[i] = words[i].toLowerCase();
+        }
         final Map<String, Integer> result = new HashMap<>();
-        for(String word: words){
-            if(word.trim().isEmpty()){
+        for(String word: lowerCaseWords){
+            if(word.trim().length() <=3){
+//        for(String word: words){
+//            if(word.trim().isEmpty() ||
+//                    Arrays.asList("i","z","na","w","się","jak","do","nie","że","to","A","I","za","po").contains(word))
+//            {
                 continue;
             }
             int number =1;
@@ -31,9 +37,14 @@ public class PanTadeusz {
         final List<Map.Entry<String, Integer>> list = new ArrayList<>(result.entrySet());
         list.sort(Map.Entry.comparingByValue());
         System.out.println("5 most common words in Pan Tadeusz");
-        for(int i=0;i<5;i++){
-            System.out.println(list.get(i));
+        int top =5;
+        while(top -->0){
+            System.out.println(list.get(list.size() - top - 1));
         }
+//        //alternatywa:
+//        for(int i=0;i<5;i++){
+//            System.out.println(list.get(i));
+//        }
 //        for(Map.Entry<String, Integer> entry : result.entrySet()){
 //            System.out.println(entry.getKey()+" : "+ entry.getValue());
         }
